a7efc7acd92de080777f088859d1ae71477192b7
Balaji <rbalajives@gmail.com>
2016-03-17 12:12:15 +0530

    log in flash message to access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user
+
+
+
+
   def new
   	@user = User.new
   end
@@ -7,7 +12,9 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
-
+  def index
+    @users = User.all
+  end
 
   def create
   	@user = User.new(user_params)
@@ -40,5 +47,10 @@ class UsersController < ApplicationController
   	params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 
-
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Log in to Continue"
+      redirect_to login_url
+    end
+  end
 end
